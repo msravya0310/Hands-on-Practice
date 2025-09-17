c6fcac26f250f94c0bb5fe2e0b19651bd9052a32
msravya0310 <msravya667@gmail.com>
2025-09-17 12:24:26 +0530

    Delete utils directory

--- utils/load_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-@st.cache_data
-def load_data(file_path="Source sales Data.xls"):
-    df = pd.read_excel(file_path)
-    df['Order Date'] = pd.to_datetime(df['Order Date'])
-    df['Ship Date'] = pd.to_datetime(df['Ship Date'])
-    return df
